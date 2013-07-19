695fe17a1297993f79c0a33827b6cbac3fb32d97
Konrad Hałas <halas.konrad@gmail.com>
2013-07-19 15:25:49 +0200

    Added setup.py draft.

---
+++ setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup
+
+setup(
+    name='django-viewtestcase',
+    version='0.0.1',
+    description='TestCase extension for Django views unit testing.',
+    author='SUNSCRAPERS',
+    packages=['viewtestcase'],
+)
