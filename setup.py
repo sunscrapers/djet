187a98ca4a6061f108ee4caf36a6d3e0642112e2
jasisz <jasisz@gmail.com>
2013-07-22 14:02:24 +0200

    Updated setup.py and docs to rst.

--- setup.py
+++ setup.py
@@ -4,6 +4,9 @@ setup(
     name='django-viewtestcase',
     version='0.0.1',
     description='TestCase extension for Django views unit testing.',
+    long_description=open('README.rst').read(),
     author='SUNSCRAPERS',
+    author_email='info@sunscrapers.com',
     packages=['viewtestcase'],
+    url='https://github.com/sunscrapers/django-viewtestcase'
 )
