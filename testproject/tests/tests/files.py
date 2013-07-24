f2ebc8d8bb281ceab3080652a96cef940447d73c
jasisz <jasisz@gmail.com>
2013-07-24 19:16:16 +0200

    Changed tests to work on older DJango versions.

--- testproject/tests/tests/files.py
+++ testproject/tests/tests/files.py
@@ -1,7 +1,5 @@
 import os
-from django.core.files.storage import default_storage
 from django.test.testcases import TestCase
-from django.test.utils import override_settings
 from djet import files
 
 
@@ -19,21 +17,21 @@ class InMemoryFilesTestCase(TestCase):
         self.assertEqual(file.name, 'test.jpg')
 
 
-@override_settings(DEFAULT_FILE_STORAGE='djet.files.InMemoryStorage')
 class InMemoryStorageTestCase(TestCase):
 
     def setUp(self):
+        self.storage = files.InMemoryStorage()
         self.file_name = 'test.txt'
         self.file = files.make_inmemory_file(self.file_name, 'Avada Kedavra')
-        default_storage.save(self.file_name, self.file)
+        self.storage.save(self.file_name, self.file)
 
     def test_file_should_not_be_saved_on_disk_when_saved(self):
         self.assertFalse(os.path.exists(self.file_name))
 
     def test_file_should_exist_when_saved(self):
-        self.assertTrue(default_storage.exists(self.file_name))
+        self.assertTrue(self.storage.exists(self.file_name))
 
     def test_file_should_be_the_same_when_opened(self):
-        uploaded_file = default_storage.open(self.file_name)
+        uploaded_file = self.storage.open(self.file_name)
 
         self.assertEqual(uploaded_file, self.file)
