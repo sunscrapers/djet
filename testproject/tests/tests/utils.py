6208f6e7a6842e4523459ab118e90a6383d5e72e
Konrad Hałas <halas.konrad@gmail.com>
2013-07-24 16:07:44 +0200

    Added refresh test.

---
+++ testproject/tests/tests/utils.py
@@ -0,0 +1,15 @@
+from django.test import TestCase
+from djet import utils
+from testproject.tests import models
+
+
+class RefreshTest(TestCase):
+
+    def test_refresh_should_update_instance_when_instance_changed(self):
+        instance = models.MockModel.objects.create(field='old')
+
+        models.MockModel.objects.filter(pk=instance.pk).update(field='new')
+
+        instance = utils.refresh(instance)
+        self.assertEqual(instance.field, 'new')
+
