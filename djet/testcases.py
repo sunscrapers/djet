6d81ea0533328c14ed134c7d6dea4e1487ea35ea
Konrad Hałas <halas.konrad@gmail.com>
2013-11-22 10:27:20 +0100

    Handle all middleware methods.

--- djet/testcases.py
+++ djet/testcases.py
@@ -1,5 +1,6 @@
 from functools import partial
 from django import test as django_test
+from django.template.response import TemplateResponse
 
 
 class RequestFactory(django_test.RequestFactory):
@@ -14,19 +15,19 @@ class RequestFactory(django_test.RequestFactory):
             shortcut = partial(self._request, method)
             setattr(self, method, shortcut)
 
-    def _request(self, method, user=None, path='',
-                 middleware_classes=None, **kwargs):
+    def _request(self, method, user=None, path='', **kwargs):
         super_method = getattr(super(RequestFactory, self), method.lower())
         request = super_method(path=path, **kwargs)
         request.user = user
-        self._process_middleware_classes(middleware_classes or [], request)
         return request
 
-    def _process_middleware_classes(self, middleware_classes, request):
-        for mw_class in self.middleware_classes + middleware_classes:
-            mw_instance = mw_class()
-            if hasattr(mw_instance, 'process_request'):
-                mw_instance.process_request(request)
+
+class MiddlewareType:
+    PROCESS_REQUEST = 'process_request'
+    PROCESS_VIEW = 'process_view'
+    PROCESS_EXCEPTION = 'process_exception'
+    PROCESS_RESPONSE = 'process_response'
+    PROCESS_TEMPLATE_RESPONSE = 'process_template_response'
 
 
 class ViewTestCase(django_test.TestCase):
@@ -38,10 +39,6 @@ class ViewTestCase(django_test.TestCase):
 
     def _pre_setup(self, *args, **kwargs):
         super(ViewTestCase, self)._pre_setup(*args, **kwargs)
-        if self.view_class:
-            self.view = self.view_class.as_view(**self.get_view_kwargs())
-        elif self.view_function:
-            self.view = self.__class__.__dict__['view_function']
         if self.factory_class:
             self.factory = self.factory_class(self.get_middleware_classes())
 
@@ -57,3 +54,70 @@ class ViewTestCase(django_test.TestCase):
         view_object.args = args
         view_object.kwargs = kwargs
         return view_object
+
+    def view(self, request, *args, **kwargs):
+        response = self._create_middlewares_processor(
+            middleware_type=MiddlewareType.PROCESS_REQUEST,
+            reverse=False,
+            return_fast=True,
+        )(request)
+        if not response:
+            response = self._create_middlewares_processor(
+                middleware_type=MiddlewareType.PROCESS_VIEW,
+                reverse=False,
+                return_fast=True,
+            )(request, self.view, args, kwargs)
+            if not response:
+                try:
+                    response = self._run_view(request, args, kwargs)
+                except Exception as exception:
+                    response = self._create_middlewares_processor(
+                        middleware_type=MiddlewareType.PROCESS_EXCEPTION,
+                        reverse=True,
+                        return_fast=True,
+                    )(request, exception)
+                    if not response:
+                        raise
+        if isinstance(response, TemplateResponse):
+            response = self._create_middlewares_processor(
+                middleware_type=MiddlewareType.PROCESS_TEMPLATE_RESPONSE,
+                reverse=True,
+                return_fast=False,
+                start_with=response,
+            )(request, response)
+        response = self._create_middlewares_processor(
+            middleware_type=MiddlewareType.PROCESS_RESPONSE,
+            reverse=True,
+            return_fast=False,
+            start_with=response,
+        )(request, response)
+        return response
+
+    def _run_view(self, request, args, kwargs):
+        if self.view_class:
+            response = self.view_class.as_view(**self.get_view_kwargs())(request, *args, **kwargs)
+        elif self.view_function:
+            response = self.__class__.__dict__['view_function'](request, *args, **kwargs)
+        return response
+
+    def _create_middlewares_processor(self, middleware_type, reverse, return_fast, start_with=None):
+        middleware_classes = self.middleware_classes or []
+        if reverse:
+            middleware_classes = reversed(middleware_classes)
+
+        def processor(*args, **kwargs):
+            result = start_with
+            for mw_class in middleware_classes:
+                if isinstance(mw_class, tuple):
+                    mw_class, mw_types = mw_class[0], mw_class[1:]
+                else:
+                    mw_types = None
+                mw_instance = mw_class()
+                if hasattr(mw_instance, middleware_type) and \
+                        (not mw_types or middleware_type in mw_types):
+                    result = getattr(mw_instance, middleware_type)(*args, **kwargs) or result
+                    if return_fast and result:
+                        break
+            return result
+
+        return processor
--- testproject/tests/tests/testcases.py
+++ testproject/tests/tests/testcases.py
@@ -1,17 +1,40 @@
-import types
 from django.core.handlers.wsgi import WSGIRequest
 from django import test as django_test
 from django.http import HttpResponse
+from django.template.response import TemplateResponse
 from django.views import generic
 from djet import testcases
 
 
 class MockMiddleware(object):
+
     def process_request(self, request):
-        request.middleware_was_here = True
+        request.process_request_was_here = True
+
+    def process_response(self, request, response):
+        response.process_response_was_here = True
+        return response
+
+    def process_template_response(self, request, response):
+        response.process_template_response_was_here = True
+        return response
+
+    def process_exception(self, request, exception):
+        respone = HttpResponse()
+        respone.process_exception_was_here = True
+        return respone
+
+
+class ProcessViewMockMiddleware(object):
+
+    def process_view(self, request, view_func, view_args, view_kwargs):
+        response = HttpResponse()
+        response.process_view_was_here = True
+        return response
 
 
 class RequestFactoryTest(django_test.TestCase):
+
     def setUp(self):
         self.factory = testcases.RequestFactory()
 
@@ -20,15 +43,6 @@ class RequestFactoryTest(django_test.TestCase):
 
         self.assertIsInstance(request, WSGIRequest)
 
-    def test_create_request_should_return_request_processes_by_middleware(self):
-        request = self.factory.get(
-            middleware_classes=[
-                MockMiddleware,
-            ]
-        )
-
-        self.assertTrue(request.middleware_was_here)
-
     def test_init_should_create_shortcuts(self):
         request = self.factory.get()
 
@@ -36,6 +50,7 @@ class RequestFactoryTest(django_test.TestCase):
 
 
 class MockView(generic.View):
+
     def mock_method(self):
         self.mock_method_called = True
 
@@ -47,6 +62,18 @@ class KwargsMockView(generic.View):
         return self.test
 
 
+class RaiseExceptionMockView(generic.View):
+
+    def get(self, *args, **kwargs):
+        raise Exception()
+
+
+class TemplateResponseMockView(generic.View):
+
+    def get(self, request, *args, **kwargs):
+        return TemplateResponse(request, 'template.html')
+
+
 def mock_function_view(request):
     return HttpResponse(status=200)
 
@@ -54,9 +81,6 @@ def mock_function_view(request):
 class ViewTestCaseTest(testcases.ViewTestCase):
     view_class = MockView
     middleware_classes = [MockMiddleware]
-
-    def test_init_should_create_view_method_when_view_class_provided(self):
-        self.assertIsInstance(self.view, types.FunctionType)
 
     def test_init_should_create_factory_instance_with_middleware_classes(self):
         self.assertIsInstance(self.factory, testcases.RequestFactory)
@@ -79,6 +103,67 @@ class ViewTestCaseTest(testcases.ViewTestCase):
         self.assertEqual(view_object.request, 'request')
         self.assertEqual(view_object.args, args)
         self.assertEqual(view_object.kwargs, kwargs)
+
+    def test_view_should_processes_request_by_middleware(self):
+        request = self.factory.get()
+
+        self.view(request)
+
+        self.assertTrue(request.process_request_was_here)
+
+    def test_view_should_processes_response_by_middleware(self):
+        request = self.factory.get()
+
+        response = self.view(request)
+
+        self.assertTrue(response.process_response_was_here)
+
+
+class ProcessExceptionMiddlewareViewTestCaseTest(testcases.ViewTestCase):
+    view_class = RaiseExceptionMockView
+    middleware_classes = [MockMiddleware]
+
+    def test_view_should_processes_exception_by_middleware(self):
+        request = self.factory.get()
+
+        response = self.view(request)
+
+        self.assertTrue(response.process_exception_was_here)
+
+
+class ProcessViewMiddlewareViewTestCaseTest(testcases.ViewTestCase):
+    view_class = MockView
+    middleware_classes = [ProcessViewMockMiddleware]
+
+    def test_view_should_processes_exception_by_middleware(self):
+        request = self.factory.get()
+
+        response = self.view(request)
+
+        self.assertTrue(response.process_view_was_here)
+
+
+class ProcessTemplateResponseMiddlewareViewTestCaseTest(testcases.ViewTestCase):
+    view_class = TemplateResponseMockView
+    middleware_classes = [MockMiddleware]
+
+    def test_view_should_processes_exception_by_middleware(self):
+        request = self.factory.get()
+
+        response = self.view(request)
+
+        self.assertTrue(response.process_template_response_was_here)
+
+
+class ProcessOnlyIndicatedMiddlewaresViewTestCaseTest(testcases.ViewTestCase):
+    view_class = RaiseExceptionMockView
+    middleware_classes = [(MockMiddleware, testcases.MiddlewareType.PROCESS_REQUEST)]
+
+    def test_view_should_not_processes_exception_by_middleware(self):
+        request = self.factory.get()
+
+        with self.assertRaises(Exception):
+            self.view(request)
 
 
 class KwargsViewTestCaseTest(testcases.ViewTestCase):
