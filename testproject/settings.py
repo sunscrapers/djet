a2933230a7c929504642b5ffbe31eee6d0330197
haxoza <prze.lewandowski@gmail.com>
2014-08-24 13:58:30 +0200

    Added explicit middleware classes for test settings in order to disable warnings in Django 1.7

--- testproject/settings.py
+++ testproject/settings.py
@@ -6,7 +6,14 @@ DATABASES = {
 
 SECRET_KEY = '_'
 
+MIDDLEWARE_CLASSES = (
+    'django.middleware.common.CommonMiddleware',
+    'django.contrib.sessions.middleware.SessionMiddleware',
+    'django.middleware.csrf.CsrfViewMiddleware',
+    'django.contrib.auth.middleware.AuthenticationMiddleware',
+    'django.contrib.messages.middleware.MessageMiddleware',
+)
+
 INSTALLED_APPS = (
     'tests',
 )
-
