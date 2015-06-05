bdd614624de969dbd6fe3c37fee6fffb276e8e1c
jasisz <jasisz@gmail.com>
2015-06-05 20:43:17 +0200

    Add content_type for image, fixes #12

--- djet/files.py
+++ djet/files.py
@@ -81,16 +81,18 @@ def create_inmemory_file(file_name='tmp.txt', content=b'', content_type=None):
     return file
 
 
-def create_inmemory_image(file_name='tmp.png', format=None, width=200, height=200):
+def create_inmemory_image(file_name='tmp.png', format=None, width=200, height=200, content_type=None):
     from PIL import Image
     if not format:
         _, extension = os.path.splitext(file_name)
         format = extension[1:].upper()
+    if not content_type:
+        content_type = 'image/{0}'.format(format)
     stream = six.BytesIO()
     size = (width, height)
     color = (255, 0, 0, 0)
     image = Image.new('RGBA', size, color)
     image.save(stream, format=format)
-    image_file = InMemoryUploadedFile(stream, None, file_name, format, stream.tell(), None)
+    image_file = InMemoryUploadedFile(stream, None, file_name, content_type, stream.tell(), None)
     image_file.seek(0)
     return image_file
