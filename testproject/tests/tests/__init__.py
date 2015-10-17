a650715636e759725785e6533f3021a60eaaabb7
haxoza <prze.lewandowski@gmail.com>
2015-10-17 15:46:01 +0200

    Updated tox and travis config files.

--- testproject/tests/tests/__init__.py
+++ testproject/tests/tests/__init__.py
@@ -1,5 +1,8 @@
-from .test_testcases import *
-from .test_assertions import *
-from .test_files import *
-from .test_utils import *
-from .test_restframework import *
+import django
+
+if django.VERSION < (1, 6):
+    from .test_testcases import *
+    from .test_assertions import *
+    from .test_files import *
+    from .test_utils import *
+    from .test_restframework import *
